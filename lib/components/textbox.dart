11dabdd1aa8b8e9ea087ec1c4c3150a3a47851dd
tytyhensley <tkh265@nyu.edu>
2020-04-02 10:50:33 +0400

    Web Deploy

--- lib/components/textbox.dart
+++ lib/components/textbox.dart
@@ -1,32 +1,38 @@
 import 'package:stopwatch/components/constants.dart';
 import 'package:flutter/material.dart';
 
-class EventTextBox extends StatelessWidget {
-  EventTextBox({
-    @required this.hint,
-    @required this.keybrd,
-    @required this.lines,
-  });
-
+class EventTextBox extends StatefulWidget {
   final String hint;
   final TextInputType keybrd;
   final int lines;
+  final TextEditingController myController;
 
+  EventTextBox(
+      {@required this.hint,
+      @required this.keybrd,
+      @required this.lines,
+      @required this.myController});
+  @override
+  _EventTextBoxState createState() => _EventTextBoxState();
+}
+
+class _EventTextBoxState extends State<EventTextBox> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(40.0),
       child: TextField(
+        controller: widget.myController,
         cursorColor: ktextColorA,
-        maxLines: lines,
+        maxLines: widget.lines,
         style: TextStyle(
           color: ktextColorA,
           fontSize: 22.0,
         ),
-        keyboardType: keybrd,
+        keyboardType: widget.keybrd,
         textCapitalization: TextCapitalization.sentences,
         decoration: InputDecoration(
-          hintText: hint,
+          hintText: widget.hint,
           hintStyle: TextStyle(
             color: Color(0xA09C9C9C),
             fontSize: 22.0,
--- lib/screens/InputPage.dart
+++ lib/screens/InputPage.dart
@@ -3,6 +3,7 @@ import 'package:stopwatch/components/textbox.dart';
 import 'package:stopwatch/components/constants.dart';
 import 'package:stopwatch/screens/StopWatch.dart';
 import 'package:stopwatch/components/watchbuttons.dart';
+import 'dart:async';
 
 class InputPage extends StatefulWidget {
   @override
@@ -10,6 +11,54 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  final textController_1 = TextEditingController();
+  final textController_2 = TextEditingController();
+  final textController_3 = TextEditingController();
+  DateTime _date = DateTime.now();
+  TimeOfDay _time = TimeOfDay.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2021),
+    );
+    final TimeOfDay picked2 = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+    if (picked != null && picked != _date) {
+      setState(() {
+        _date = picked;
+        _time = picked2;
+        textController_2.text = _date.year.toString() +
+            '-' +
+            _date.month.toString() +
+            '-' +
+            _date.day.toString() +
+            ' ' +
+            _time.hour.toString() +
+            ':' +
+            _time.minute.toString();
+      });
+    }
+  }
+
+  checkTextFieldEmptyOrNot() {
+    String text1, text2, text3;
+
+    text1 = textController_1.text;
+    text2 = textController_2.text;
+    text3 = textController_3.text;
+
+    if (text1 == '' || text2 == '' || text3 == '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,37 +69,68 @@ class _InputPageState extends State<InputPage> {
           style: kAppTextStyle,
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          EventTextBox(
-            hint: 'Event Name',
-            keybrd: TextInputType.text,
-            lines: 1,
-          ),
-          EventTextBox(
-            hint: 'Event Date',
-            keybrd: TextInputType.datetime,
-            lines: 1,
-          ),
-          EventTextBox(
-            hint: 'Event Description',
-            keybrd: TextInputType.text,
-            lines: 6,
-          ),
-          WatchButton(
-            buttontitle: 'START EVENT',
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => StopWatch(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            EventTextBox(
+              hint: 'Event Name',
+              keybrd: TextInputType.text,
+              lines: 1,
+              myController: textController_1,
+            ),
+            InkWell(
+              onTap: () {
+                _selectDate(context);
+              },
+              child: IgnorePointer(
+                child: EventTextBox(
+                  hint: 'Event Date',
+                  keybrd: TextInputType.datetime,
+                  lines: 1,
+                  myController: textController_2,
                 ),
-              );
-            },
-            textcolor: ktextColorA,
-          ),
-        ],
+              ),
+            ),
+            EventTextBox(
+              hint: 'Event Description',
+              keybrd: TextInputType.text,
+              lines: 6,
+              myController: textController_3,
+            ),
+            Builder(
+              builder: (context) => WatchButton(
+                buttontitle: 'START EVENT',
+                onTap: () {
+                  if (checkTextFieldEmptyOrNot() == true) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => StopWatch(),
+                      ),
+                    );
+                  } else {
+                    Scaffold.of(context).showSnackBar(
+                      SnackBar(
+                        duration: Duration(seconds: 1),
+                        backgroundColor: kbuttonColor,
+                        content: Text(
+                          'Please fill in all the text fields',
+                          style: TextStyle(
+                            color: ktextColorA,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                textcolor: ktextColorA,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
